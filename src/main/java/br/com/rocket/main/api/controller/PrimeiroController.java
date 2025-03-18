b29e002c56006e8c8886c98351ac82f605eb6827
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2025-03-18 10:59:25 -0300

    parametros do controller

--- src/main/java/br/com/rocket/main/api/controller/PrimeiroController.java
+++ src/main/java/br/com/rocket/main/api/controller/PrimeiroController.java
@@ -1,9 +1,8 @@
 package br.com.rocket.main.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequestMapping("/")
 @RestController
@@ -13,6 +12,17 @@ public class PrimeiroController {
     @GetMapping("/primeiroMetodo/{id}")
     public String test(@PathVariable(name = "id")  String id){
         return "ola mundo" + id;
+    }
+
+    @GetMapping("/metodoqueryparams")
+    public String metodocomqueryparams(@RequestParam String id){
+        return "o query é : " +  id;
+    }
+
+
+    @GetMapping("/metodoqueryparams2")
+    public String metodocomqueryparams(@RequestParam Map<String,String> allparams){
+        return "o query é : " +  allparams.entrySet();
     }
 
 }
